6c3e04982b9cd9bd8e7d880a9266594e634061df
Alana Soares <abs_alana@hotmail.com>
2022-09-16 21:36:45 -0300

    Adicionando Arquivo DML-Updates.sql
    Arquivo contendo atualização dos itens da tabela produtos.

---
+++ DML-Updates.sql
@@ -0,0 +1,15 @@
+use todo5;
+
+update produtos set material = "tecido e borracha" where idProduto = 1;
+
+update produtos set preco = 79.99 where idProduto = 15;
+
+update produtos set tamanho = "GG" where idProduto = 9;
+
+update produtos set material = "tecido e borracha" where idProduto = 5;
+
+update produtos set preco = 199.99 where idProduto = 3;
+
+update produtos set categoria = "Camisetas de Banda" where idProduto = 9
+
+
