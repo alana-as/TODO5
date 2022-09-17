342e5936e2c75b207ff89a737615be3dc0158ba4
Alana Soares <abs_alana@hotmail.com>
2022-09-16 21:34:30 -0300

    Adicionado Arquivo DML-Inserts.sql
    Inserção de 15 produtos na tabela produtos.

---
+++ DML-Inserts.sql
@@ -0,0 +1,18 @@
+use todo5;
+
+insert into produtos(sku, descricao, categoria, material, tamanho, fornecedor, preco) 
+values(230001, "Tennis AllStar Azul", "Calcados", "Tecido", 35, "Converse", 159.99),
+(230002, "Tennis AllStar Amarelo", "Calcados", "Tecido", 36, "Converse", 159.99),
+(230003, "Tennis AllStar Verde", "Calcados", "Tecido", 35, "Converse", 159.99),
+(230004, "Tennis AllStar Vermelho", "Calcados", "Tecido", 38, "Converse", 159.99),
+(230005, "Tennis AllStar Preto", "Calcados", "Tecido", 37, "Converse", 159.99),
+(230006, "Camiseta Fresno", "Camisetas de Banda", "Algodao", "P", "Lojas Renner", 99.99),
+(230007, "Camiseta GreenDay", "Camisetas de Banda", "Algodao", "M", "Lojas Renner", 99.99),
+(230008, "Camiseta Nirvana", "Camisetas de Banda", "Algodao", "G", "Lojas Renner", 99.99),
+(230009, "Camiseta Paramore", "Camisetas de Bafornecedornda", "Algodao", "PP", "Lojas Renner", 99.99),
+(230010, "Camiseta RBD", "Camisetas de Banda", "Algodao", "P", "Lojas Renner", 99.99),
+(230011, "Bone Nike", "Acessorios", "Plastico e Algodao", "Unico", "Nike", 69.99),
+(230012, "Bone Adidas", "Acessorios", "Plastico e Algodao", "Unico", "Adidas", 69.99),
+(230013, "Bone Fresno", "Acessorios", "Plastico e Algodao", "Unico", "FresnoShop", 69.99),
+(230014, "Bone Fila", "Acessorios", "Plastico e Algodao", "Unico", "Fila", 69.99),
+(230015, "Bone Nirvana", "Acessorios", "Plastico e Algodao", "Unico", "Lojas Renner", 69.99)
